4abda383ebc7127b957afa8e6149fa0eda1c1e07
Roman <borisenko0893@gmail.com>
2024-08-18 12:42:00 +0300

    add tests

--- 7.3/jest/test/unit/sortByName.test.js
+++ 7.3/jest/test/unit/sortByName.test.js
@@ -15,3 +15,24 @@ describe("Books names test suit", () => {
     ]);
   });
 });
+
+describe("Books names test suit with one element", () => {
+  it("Books names should be one element", () => {
+    expect(sorting.sortByName(["Гарри Поттер"])).toEqual(["Гарри Поттер"]);
+  });
+});
+
+describe("Books names test suit reght",() =>{
+  it("Wuthout pararms thows exception", () =>{
+    expect(() => sorting.sortByName()).toThrow(TypeError);
+  });
+}
+);
+
+describe("Books names test suit two elements are the same", () => {
+  it("Books names should two elements are the same", () => {
+    expect(
+      sorting.sortByName(["Гарри Поттер", "Властелин Колец", "Гарри Поттер"])
+    ).toEqual(["Властелин Колец", "Гарри Поттер", "Гарри Поттер"]);
+  });
+});
