2bd02539d27cb60ace776097e82c8984745994a9
Roman <borisenko0893@gmail.com>
2024-08-18 16:05:40 +0300

    add code

---
+++ 7.3/playwright/tests/test.spec.js
@@ -0,0 +1,39 @@
+const { test, expect } = require("@playwright/test");
+const {
+  username,
+  password, 
+  unknownUsername,
+  unkwnownPassword,
+} = require("./../user");
+
+test("Good test", async ({ page }) => {
+  await page.goto("https://netology.ru/?modal=sign_in");
+  await page.getByPlaceholder("Email").click();
+  await page.getByPlaceholder("Email").fill(username);
+  await page.getByPlaceholder("Пароль").click();
+  await page.getByPlaceholder("Пароль").fill(password);
+  await page.getByTestId("login-submit-btn").click();
+
+  const h2Text = await page.innerText("h2");
+  await expect(h2Text).toBe("Моё обучение");
+});
+
+
+test("Authorisation failed", async ({ page }) => {
+
+await page.goto("https://netology.ru/?modal=sign_in");
+await page.getByPlaceholder("Email").click();
+await page.getByPlaceholder("Email").fill(unknownUsername);
+await page.getByPlaceholder("Пароль").click();
+await page.getByPlaceholder("Пароль").fill(unkwnownPassword);
+await page.getByTestId("login-submit-btn").click();
+
+
+
+  const error = await page.locator('[data-testid="login-error-hint"]');
+  await expect(error).toHaveText("Вы ввели неправильно логин или пароль.");
+});
+
+
+
+
